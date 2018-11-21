584a20eda1cd6f5639a0329d06c3cbaaff88609b
ltanh99 <43319098+ltanh99@users.noreply.github.com>
2018-11-22 01:06:42 +0700

    Add files via upload

--- src/uet/oop/bomberman/entities/tile/item/Item.java
+++ src/uet/oop/bomberman/entities/tile/item/Item.java
@@ -4,8 +4,37 @@ import uet.oop.bomberman.entities.tile.Tile;
 import uet.oop.bomberman.graphics.Sprite;
 
 public abstract class Item extends Tile {
-
+        protected int _duration = -1; // -1 is infinite, duration in lifes
+	protected boolean _active = false;
 	public Item(int x, int y, Sprite sprite) {
 		super(x, y, sprite);
 	}
+        public abstract void setValues();
+	
+	public void removeLive() {
+		if(_duration > 0)
+			_duration--;
+		
+		if(_duration == 0)
+			_active = false;
+	}
+	
+	public int getDuration() {
+		return _duration;
+	}
+	public void setDuration(int duration) {
+		this._duration = duration;
+	}
+
+	public boolean isActive() {
+		return _active;
+	}
+
+	public void setActive(boolean active) {
+		this._active = active;
+	}
+
+    public void destroy() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }

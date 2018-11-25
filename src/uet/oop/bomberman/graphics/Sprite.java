8162811121fcf27ae2f8256a99fbfade2b0ca3ba
ltanh99 <43319098+ltanh99@users.noreply.github.com>
2018-11-25 21:22:41 +0700

    Add files via upload

--- src/uet/oop/bomberman/graphics/Sprite.java
+++ src/uet/oop/bomberman/graphics/Sprite.java
@@ -28,26 +28,26 @@ public class Sprite {
 	| Bomber Sprites
 	|--------------------------------------------------------------------------
 	 */
-	public static Sprite player2_up = new Sprite(16, 3, 7, SpriteSheet.tiles, 12, 16);
-	public static Sprite player2_down = new Sprite(16, 5, 1, SpriteSheet.tiles, 12, 15);
-	public static Sprite player2_left = new Sprite(16, 6, 1, SpriteSheet.tiles, 10, 15);
-	public static Sprite player2_right = new Sprite(16, 4, 1, SpriteSheet.tiles, 10, 16);
+	public static Sprite player2_up = new Sprite(16, 0, 12, SpriteSheet.tiles, 12, 16);
+	public static Sprite player2_down = new Sprite(16, 2, 12, SpriteSheet.tiles, 12, 15);
+	public static Sprite player2_left = new Sprite(16, 3, 12, SpriteSheet.tiles, 10, 15);
+	public static Sprite player2_right = new Sprite(16, 1, 12, SpriteSheet.tiles, 10, 16);
 	
-	public static Sprite player2_up_1 = new Sprite(16, 3, 8, SpriteSheet.tiles, 12, 16);
-	public static Sprite player2_up_2 = new Sprite(16, 3, 9, SpriteSheet.tiles, 12, 15);
+	public static Sprite player2_up_1 = new Sprite(16, 0, 13, SpriteSheet.tiles, 12, 16);
+	public static Sprite player2_up_2 = new Sprite(16, 0, 14, SpriteSheet.tiles, 12, 15);
 	
-	public static Sprite player2_down_1 = new Sprite(16, 4, 3, SpriteSheet.tiles, 12, 15);
-	public static Sprite player2_down_2 = new Sprite(16, 5, 4, SpriteSheet.tiles, 12, 16);
+	public static Sprite player2_down_1 = new Sprite(16, 2, 13, SpriteSheet.tiles, 12, 15);
+	public static Sprite player2_down_2 = new Sprite(16, 2, 14, SpriteSheet.tiles, 12, 16);
 	
-	public static Sprite player2_left_1 = new Sprite(16, 5, 3, SpriteSheet.tiles, 11, 16);
-	public static Sprite player2_left_2 = new Sprite(16, 6, 4, SpriteSheet.tiles, 12 ,16);
+	public static Sprite player2_left_1 = new Sprite(16, 3, 13, SpriteSheet.tiles, 11, 16);
+	public static Sprite player2_left_2 = new Sprite(16, 3, 14, SpriteSheet.tiles, 12 ,16);
 	
-	public static Sprite player2_right_1 = new Sprite(16, 3, 3, SpriteSheet.tiles, 11, 16);
-	public static Sprite player2_right_2 = new Sprite(16, 4, 4, SpriteSheet.tiles, 12, 16);
+	public static Sprite player2_right_1 = new Sprite(16, 1, 13, SpriteSheet.tiles, 11, 16);
+	public static Sprite player2_right_2 = new Sprite(16, 1, 14, SpriteSheet.tiles, 12, 16);
 	
-	public static Sprite player2_dead1 = new Sprite(16, 4, 2, SpriteSheet.tiles, 14, 16);
-	public static Sprite player2_dead2 = new Sprite(16, 5, 2, SpriteSheet.tiles, 13, 15);
-	public static Sprite player2_dead3 = new Sprite(16, 6, 2, SpriteSheet.tiles, 16, 16);
+	public static Sprite player2_dead1 = new Sprite(16, 4, 14, SpriteSheet.tiles, 14, 16);
+	public static Sprite player2_dead2 = new Sprite(16, 5, 14, SpriteSheet.tiles, 13, 15);
+	public static Sprite player2_dead3 = new Sprite(16, 6, 14, SpriteSheet.tiles, 16, 16);
 	
         public static Sprite player_up = new Sprite(16, 0, 0, SpriteSheet.tiles, 12, 16);
 	public static Sprite player_down = new Sprite(16, 2, 0, SpriteSheet.tiles, 12, 15);
